7ac08809c6b78b79f77c4bccd3ed3f3982d5c3a9
coelhocaique <coelhocaiquesantos@hotmail.com>
2022-02-07 17:47:21 -0300

    changes service url

--- src/app/_helpers/constants.ts
+++ src/app/_helpers/constants.ts
@@ -1,4 +1,4 @@
-export const URL_GATEWAY = "http://host.docker.internal:8083"
+export const URL_GATEWAY = "http://127.0.0.1:8083"
 
 export const MONTH_NAMES = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"
